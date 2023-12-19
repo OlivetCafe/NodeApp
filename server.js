1a7be9c814ed33441fd299b889bfa3cbe7d2ef0a
Felipe Heide <felipe.heide@live.com>
2023-12-19 12:52:30 -0600

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
@@ -21,8 +24,10 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
-//https://www.youtube.com/watch?v=qj2oDkvc4dQ&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=7
-// 12:49
+//https://www.youtube.com/watch?v=esy4nRuShl8&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=6
+// 19:48 error MongooseError: Model.prototype.save() no longer accepts a callback
+//    at model.save (C:\Users\Usuario\OneDrive\Desktop\expressServer\node_modules\mongoose\lib\model.js:517:11)
